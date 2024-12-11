1e0fb8949559de3b3743fee920d31248a15ee36e
Bryce199805 <bryce199805@gmail.com>
2024-12-11 11:01:53 +0800

    调整样例

--- main.cc
+++ main.cc
@@ -18,7 +18,8 @@ struct parser {
     sqltoast::parse_input_t subject;
     sqltoast::parse_result_t res;
 
-    parser(sqltoast::parse_options_t &opts, const std::string &input) : opts(opts),subject(input.cbegin(), input.cend()) {
+    parser(sqltoast::parse_options_t& opts, const std::string& input) : opts(opts),
+                                                                        subject(input.cbegin(), input.cend()) {
     }
 
     void operator()() {
@@ -29,21 +30,34 @@ struct parser {
 
 int main() {
     std::string input =
-            "SELECT t1.a, t1.b, t2.c FROM DB1.TB1 AS t1 JOIN (SELECT * FROM DB2.TB2 JOIN DB3.TB3 ON TB2.id = TB3.id) t2 ON t1.a = t2.b";
+        "SELECT t1.a, t1.b, t2.c FROM DB1.TB1 AS t1 JOIN (SELECT * FROM DB2.TB2 JOIN DB3.TB3 ON TB2.id = TB3.id) t2 ON t1.a = t2.b";
 
-    sqltoast::parse_options_t opts = {sqltoast::SQL_DIALECT_ANSI_1992, false};
+    sqltoast::parse_options_t opts = {sqltoast::SQL_DIALECT_ANSI_1999, false};
     parser p(opts, input);
 
     auto dur = measure<std::chrono::nanoseconds>::execution(p);
     sqltoaster::printer ptr(p.res, std::cout);
+    // ptr.output_format = sqltoaster::OUTPUT_FORMAT_YAML;
 
-    if (p.res.code == sqltoast::PARSE_OK)
-        std::cout << ptr << std::endl;
+    std::vector<std::string> res_lst;
+
+    if (p.res.code == sqltoast::PARSE_OK) {
+        ptr.process_statements();
+        if (ptr.statement_node_count()) {
+            sqltoaster::mapping_t statements;
+            statements.setattr("statements", ptr.statements);
+            get_table(statements, res_lst);
+        }
+    }
     else if (p.res.code == sqltoast::PARSE_INPUT_ERROR)
         std::cout << "Input error: " << p.res.error << std::endl;
     else {
         std::cout << "Syntax error." << std::endl;
         std::cout << p.res.error << std::endl;
+    }
+
+    for (auto& i : res_lst) {
+        std::cout << i << std::endl;
     }
     return 0;
 }
