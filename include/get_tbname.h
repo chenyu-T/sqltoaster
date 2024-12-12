3f98e83d2541cdc151a8855cb16e5b7f53bb2d20
chenyu-T <954972374@qq.com>
2024-12-12 21:30:37 +0800

    修改get_tbname方法，增加其他语句支持

--- include/get_tbname.h
+++ include/get_tbname.h
@@ -1,4 +1,3 @@
-
 #ifndef GET_TBNAME_H
 #define GET_TBNAME_H
 
@@ -9,42 +8,71 @@
 #include "measure.h"
 #include "printer.h"
 
-struct parser {
-    sqltoast::parse_options_t opts;
-    sqltoast::parse_input_t subject;
-    sqltoast::parse_result_t res;
+namespace sqltoaster {
+    struct parser {
+        sqltoast::parse_options_t opts;
+        sqltoast::parse_input_t subject;
+        sqltoast::parse_result_t res;
 
-    parser(sqltoast::parse_options_t &opts, const std::string &input) : opts(opts), subject(input.cbegin(), input.cend()) {
-    }
-
-    void operator()() {
-        res = sqltoast::parse(subject, opts);
-    }
-};
-
-bool getTBName(std::string input, std::vector<std::string> &res_lst) {
-    sqltoast::parse_options_t opts = {sqltoast::SQL_DIALECT_ANSI_1999, false};
-    parser p(opts, input);
-    auto dur = measure<std::chrono::nanoseconds>::execution(p);
-    sqltoaster::printer ptr(p.res, std::cout);
-    // ptr.output_format = sqltoaster::OUTPUT_FORMAT_YAML;
-
-    if (p.res.code == sqltoast::PARSE_OK) {
-        ptr.process_statements();
-        if (ptr.statement_node_count()) {
-            sqltoaster::mapping_t statements;
-            statements.setattr("statements", ptr.statements);
-            get_table(statements, res_lst);
+        parser(sqltoast::parse_options_t &opts, const std::string &input) : opts(opts), subject(input.cbegin(), input.cend()) {
         }
-    } else if (p.res.code == sqltoast::PARSE_INPUT_ERROR) {
-        std::cout << "Input error: " << p.res.error << std::endl;
-        return false;
-    } else {
-        std::cout << "Syntax error." << std::endl;
-        std::cout << p.res.error << std::endl;
-        return false;
-    }
-    return true;
-}
+        void operator()() {
+            res = sqltoast::parse(subject, opts);
+        }
+    };
 
+    bool getTBName(std::string input, std::vector<std::string> &res_lst) {
+        sqltoast::parse_options_t opts = {sqltoast::SQL_DIALECT_ANSI_1999, false};
+        parser p(opts, input);
+        auto dur = measure<std::chrono::nanoseconds>::execution(p);
+        sqltoaster::printer ptr(p.res, std::cout);
+        // ptr.output_format = sqltoaster::OUTPUT_FORMAT_YAML;
+
+        if (p.res.code == sqltoast::PARSE_OK) {
+            for (const auto &stmt: p.res.statements) {
+                switch (stmt->type) {
+                    case sqltoast::STATEMENT_TYPE_SELECT: {
+                        // 处理 SELECT 的表信息
+                        ptr.process_statements();
+                        if (ptr.statement_node_count()) {
+                            sqltoaster::mapping_t statements;
+                            statements.setattr("statements", ptr.statements);
+                            get_table(statements, res_lst);
+                        }
+                        break;
+                    }
+                    case sqltoast::STATEMENT_TYPE_INSERT: {
+                        const auto &insert_stmt = static_cast<const sqltoast::insert_statement_t &>(*stmt);
+                        // 提取 INSERT 的目标表
+                        res_lst.push_back(std::string(insert_stmt.table_name.start, insert_stmt.table_name.end));
+                        break;
+                    }
+                    case sqltoast::STATEMENT_TYPE_UPDATE: {
+                        const auto &update_stmt = static_cast<const sqltoast::update_statement_t &>(*stmt);
+                        // 提取 UPDATE 的目标表
+                        res_lst.push_back(std::string(update_stmt.table_name.start, update_stmt.table_name.end));
+                        break;
+                    }
+                    case sqltoast::STATEMENT_TYPE_DELETE: {
+                        const auto &delete_stmt = static_cast<const sqltoast::delete_statement_t &>(*stmt);
+                        // 提取 DELETE 的目标表
+                        res_lst.push_back(std::string(delete_stmt.table_name.start, delete_stmt.table_name.end));
+                        break;
+                    }
+                    default:
+                        std::cout << "Unsupported statement type: " << stmt->type << std::endl;
+                }
+            }
+
+        } else if (p.res.code == sqltoast::PARSE_INPUT_ERROR) {
+            std::cout << "Input error: " << p.res.error << std::endl;
+            return false;
+        } else {
+            std::cout << "Syntax error." << std::endl;
+            std::cout << p.res.error << std::endl;
+            return false;
+        }
+        return true;
+    }
+}
 #endif //GET_TBNAME_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 
 int main() {
     std::vector<std::string> res_lst;
-    std::string input = "SELECT t1.a, t1.b, t2.c FROM DB1.TB1 AS t1 JOIN (SELECT * FROM DB2.TB2 JOIN DB3.TB3 ON TB2.id = TB3.id) t2 ON t1.a = t2.b";
-    if(getTBName(input, res_lst)) {
+    std::string select_input = "SELECT t1.a, t1.b, t2.c FROM DB1.TB1 AS t1 JOIN (SELECT * FROM DB2.TB2 JOIN DB3.TB3 ON TB2.id = TB3.id) t2 ON t1.a = t2.b";
+    std::string insert_input = "insert into BASIC_SHIP_INFORMATION_DB.SISTER_SHIP_INFO (SHIP_DATA_ID, SISTER_DATA_ID, SHIP_NAME) values (7082006, 7082007, 'test No1')";
+    std::string update_input = "UPDATE db3.table3 SET column1 = 'value' WHERE column2 = 'key'";
+    std::string delete_input = "DELETE FROM db4.table4 WHERE column1 = 'value'";
+    if(sqltoaster::getTBName(delete_input, res_lst)) {
         for (auto& i : res_lst) {
             std::cout << i << std::endl;
         }
